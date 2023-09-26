c21a67f88695b3149eef5040f9f0ac075fb8998a
ingsoft3ucc <140459109+ingsoft3ucc@users.noreply.github.com>
2023-09-26 19:33:43 -0300

    Add files via upload
    Initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Reservation reservation = new Reservation();
+        User user=new User();
+        user.IsAdmin=true;
+        bool result=reservation.CanBeCancelledBy(user);
+        Console.WriteLine(result);
+    }
+}
+
+
+
